04b25901519f0583f669cfd349ba704d4a173f08
Ryan Fleck <ryan.fleck@protonmail.com>
2019-06-16 19:22:15 -0400

    Refactor face.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const Telegraf = require('telegraf')
-const Express = require('express')
-
-const PORT = process.env.PORT
-Express().get("*", (req,res)=>{
-    res.json({message:"Hi, it's Rath."})
-}).listen(PORT, ()=>{console.log("Hello Heroku")})
+const Face = require('./utils/face')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((c) => c.reply("Welcome!"));
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.hears(['hi','hello','Hi','Hello'], (ctx) => ctx.reply('Hello there.'))
+bot.hears('Rath', (ctx) => ctx.reply('You called?'))
 bot.command('marco', (ctx) => ctx.reply('Polo.'))
 
 bot.launch();
+
+// Heroku stopgap.
+Face.start();
---
+++ utils/face.js
@@ -0,0 +1,10 @@
+const Express = require('express')
+
+module.exports = class Face {
+    static start() {
+        const PORT = process.env.PORT || 3000
+        Express().get("*", (req, res) => {
+            res.json({ message: "Hi, it's Rath." })
+        }).listen(PORT, () => { console.log("Hello Heroku") })
+    }
+}
