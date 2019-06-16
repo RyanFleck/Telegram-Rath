17fcdb96e9b07291d0bcc5a94fbd281f332c3c4b
Ryan Fleck <ryan.fleck@protonmail.com>
2019-06-16 19:58:38 -0400

    Remove error causing code.

--- src/main.js
+++ src/main.js
@@ -6,16 +6,13 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((c) => c.reply("Welcome!"));
 bot.on('sticker', (c) => c.reply('lol'))
 
-
-
-bot.hears(['hi','hello','Hi','Hello'], (c) => c.reply('Hello there.'))
+bot.hears(['hi', 'hello', 'Hi', 'Hello'], (c) => c.reply('Hello there.'))
 bot.hears(/^.+([Rr]ath)/gm, (c) => c.reply('lmao'))
 bot.hears(/([Rr]ath)/gm, (c) => c.reply('Alright, I\'ll take a look...'))
 bot.command('marco', (c) => c.reply('Polo.'))
 
 bot.launch();
 
-bot.command('test')
 
-// Heroku stopgap.
+// Heroku stopgap. Start last.
 Face.start();
