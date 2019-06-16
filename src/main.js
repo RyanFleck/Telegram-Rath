ca4a042b28cb48a05d265a6c9b0a4fee5dae57ce
Ryan Fleck <ryan.fleck@protonmail.com>
2019-06-16 19:42:36 -0400

    Refactor

--- src/main.js
+++ src/main.js
@@ -6,11 +6,15 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((c) => c.reply("Welcome!"));
 bot.on('sticker', (c) => c.reply('lol'))
 
+
+
 bot.hears(['hi','hello','Hi','Hello'], (c) => c.reply('Hello there.'))
-bot.hears('Rath', (c) => c.reply('You called?'))
+bot.hears(/([Rr]ath)/gm, (c) => c.reply('Hm?'))
 bot.command('marco', (c) => c.reply('Polo.'))
 
 bot.launch();
+
+bot.command('test')
 
 // Heroku stopgap.
 Face.start();
