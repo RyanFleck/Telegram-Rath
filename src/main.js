263415ee4d9b4bf8c6a36fde789859a4d93201d2
Ryan Fleck <ryan.fleck@protonmail.com>
2019-06-17 07:35:23 -0400

    Attempt to use different heroku fix.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const Telegraf = require('telegraf')
-const Face = require('./utils/face')
+//const Face = require('./utils/face')
 const Yes = require('./utils/yes')
 const Haha = require('./utils/haha')
 
@@ -8,13 +8,7 @@ console.time("start")
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((c) => c.reply("Welcome!"));
-bot.on('sticker', (c) => c.reply('lol'))
-
-/*
-bot.on('message', (c) => {
-    console.log(c.message)
-})
-*/
+bot.on('sticker', (c) => c.reply(Haha.haha()))
 
 bot.hears(['hi', 'hello', 'Hi', 'Hello'], (c) => c.reply(`Hello there, ${c.from.first_name}.`))
 bot.hears(/^.+([Rr]ath)/gm, (c) => {
@@ -31,6 +25,4 @@ bot.hears(/^([Rr]ath)/gm, (c) => {
 bot.command('marco', (c) => c.reply('Polo.'))
 
 bot.launch();
-
-// Heroku stopgap. Start last.
-Face.start();
+bot.startWebhook('/secret-path', null, (process.env.PORT || 5000))
